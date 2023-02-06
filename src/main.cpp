6b72a63b5ce9ca0d28c38484e97903432ec2d92a
Vladas Zakrevskis <146100@gmail.com>
2023-02-06 20:32:09 +0200

    astyle

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,25 @@
 using namespace std;
 
 extern "C" {
-  int _write(int fd, char *ptr, int len) {
-    (void) fd;
-    return Serial.write(ptr, len);
-  }
+    int _write(int fd, char *ptr, int len) {
+        (void) fd;
+        return Serial.write(ptr, len);
+    }
 }
 
 
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600); // open the serial port at 9600 bps:
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+    // put your main code here, to run repeatedly:
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(1000);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(1000);                      // wait for a second
 
     cout << "Ultra skogggo4" << " multi pedven" << endl << endl;
-
-  }
+}
