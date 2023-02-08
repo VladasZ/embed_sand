e3e7cead3e852450b0d559cd4ed35a4d5e289f99
Vladas Zakrevskis <146100@gmail.com>
2023-02-08 04:07:05 +0200

    created app empty screen

--- remote/src/main.rs
+++ remote/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use test_engine::{
+    gm::flat::Size,
+    paths::home,
+    ui,
+    ui::{refs::Own, View},
+    view, App, AppCore, MakeApp,
+};
+
+#[view]
+#[derive(Default)]
+struct MainView {}
+
+struct RemoteApp {
+    core: AppCore,
+}
+
+impl App for RemoteApp {
+    fn screen_size() -> Size
+    where Self: Sized {
+        (1000, 1000).into()
+    }
+
+    fn assets_path() -> PathBuf
+    where Self: Sized {
+        home().join("remote/remote/deps/test_engine")
+    }
+
+    fn make_root_view() -> Own<dyn View>
+    where Self: Sized {
+        Own::<MainView>::default()
+    }
+
+    fn with_core(core: AppCore) -> Self
+    where Self: Sized {
+        Self { core }
+    }
+
+    fn core(&mut self) -> &mut AppCore {
+        &mut self.core
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    RemoteApp::make_app().launch();
 }
